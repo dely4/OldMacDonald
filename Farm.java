3c1c8fc821d66ecc02e62f9e1bc08c960b1eb0ea
dely4 <dely@s.sfusd.edu>
2018-10-22 14:09:20 -0700

    New farm

--- Farm.java
+++ Farm.java
@@ -1,4 +1,18 @@
 class Farm 
 {     
-   //your code here
+   private Animal[] aBunchoOfAnimals;
+   public Farm(){
+
+   		aBunchoOfAnimals = new Animal[3];
+   		aBunchoOfAnimals[0] = new Cow("cow", "moo");
+   		aBunchoOfAnimals[1] = new Chick("chick", "cluck");
+   		aBunchoOfAnimals[2] = new Pig("pig", "oink");
+   }
+
+   public void animalSounds(){
+   		for(int i = 0; i < aBunchoOfAnimals.length; i++){
+   			System.out.println(aBunchoOfAnimals[i].getType() + " goes " + aBunchoOfAnimals[i].getSound());
+   		}
+   }
+
 }
